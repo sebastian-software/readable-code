1f898e88f83b6aa715e4b13af133d5c41094d2f2
Sebastian Werner <s.werner@sebastian-software.de>
2016-11-04 11:44:24 +0100

    Added basic gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const stylelint = require('gulp-stylelint');
+
+gulp.task('lint:js', () =>
+  gulp.src('src/**/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format('node_modules/eslint-formatter-pretty'))
+);
+
+gulp.task('lint:css', () =>
+  gulp.src('src/**/*.css')
+    .pipe(stylelint({
+      reporters: [
+        {formatter: 'string', console: true}
+      ]
+    }))
+    .pipe(eslint.format('node_modules/eslint-formatter-pretty'))
+);
+
