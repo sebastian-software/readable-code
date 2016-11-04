b8f005cbe314a2b5a39dbb266e95ec23ed2b8314
Sebastian Werner <s.werner@sebastian-software.de>
2016-11-04 13:20:44 +0100

    Tweaked linting to automatically figure out git tracked files

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,27 @@
+/* eslint-disable prefer-arrow-callback */
+
 const gulp = require("gulp")
 const eslint = require("gulp-eslint")
 const stylelint = require("gulp-stylelint")
+const exec = require("child_process").execSync
 
-gulp.task("lint:js", () =>
-  gulp.src([ "*.js", "src/**/*.js" ])
+function getGitFiles(regexp) {
+  var gitFiles = exec("git ls-files").toString().trim().split("\n")
+  return gitFiles.filter(function(fileName) { return regexp.exec(fileName) })
+}
+
+gulp.task("lint:js", function() {
+  return gulp.src(getGitFiles(/\.(js|jsx)$/))
     .pipe(eslint())
     .pipe(eslint.format("node_modules/eslint-formatter-pretty"))
-)
+})
 
-gulp.task("lint:css", () =>
-  gulp.src([ "src/**/*.css" ])
+gulp.task("lint:css", function() {
+  return gulp.src(getGitFiles(/\.(css|sass|scss|sss)$/))
     .pipe(stylelint({
       reporters: [
         { formatter: "string", console: true }
       ]
     }))
     .pipe(eslint.format("node_modules/eslint-formatter-pretty"))
-)
+})
