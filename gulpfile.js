e98113d8452d5d883ed542e0bf60a2d8a7221d1e
Sebastian Werner <s.werner@sebastian-software.de>
2016-11-04 11:47:40 +0100

    Added top level JS to eslint checks

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ const eslint = require("gulp-eslint")
 const stylelint = require("gulp-stylelint")
 
 gulp.task("lint:js", () =>
-  gulp.src("src/**/*.js")
+  gulp.src([ "*.js", "src/**/*.js" ])
     .pipe(eslint())
     .pipe(eslint.format("node_modules/eslint-formatter-pretty"))
 )
 
 gulp.task("lint:css", () =>
-  gulp.src("src/**/*.css")
+  gulp.src([ "src/**/*.css" ])
     .pipe(stylelint({
       reporters: [
         { formatter: "string", console: true }
@@ -17,4 +17,3 @@ gulp.task("lint:css", () =>
     }))
     .pipe(eslint.format("node_modules/eslint-formatter-pretty"))
 )
-
