2eb3c71df1c677aa7dbd9b3929e371a43cae2613
Sebastian Werner <s.werner@sebastian-software.de>
2017-01-24 14:28:41 +0100

    Added compat plugin for eslint

--- gulpfile.js
+++ gulpfile.js
@@ -53,8 +53,8 @@ gulp.task("fix:css", function() {
     // TODO: Support different syntax/formats
     postcss([ stylefmt ]).process(fileContent).then(function(result) {
       return fs.writeFileSync(fileName, result.css, "utf-8")
-    }).catch(function(err) {
-      throw new Error(`Error during CSS formatting: ${ err}`)
+    }).catch(function(error) {
+      throw new Error(`Error during CSS formatting: ${error}`)
     })
   })
 })
