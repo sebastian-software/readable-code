9bbb3d907f106aa7cd2707b17c4e4e7bfb4cd0b4
Sebastian Werner <s.werner@sebastian-software.de>
2017-04-20 14:18:13 +0200

    Code cleanup

--- src/tasks/javascript.js
+++ src/tasks/javascript.js
@@ -5,38 +5,43 @@ import plumber from "gulp-plumber"
 
 import { getGitFiles } from "./core"
 
+const SRC_SCRIPTS = /\.(mjs|js|jsx)$/
+const SRC_CONFIG = { base: "." }
+const PRETTIER_CONFIG = {
+  printWidth: 111,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: false,
+  trailingComma: "none",
+  jsxBracketSameLine: true,
+  bracketSpacing: true,
+  semi: false
+}
+const ESLINT_FORMATTER = "node_modules/eslint-formatter-pretty"
+
 gulp.task("lint:js", () => {
   return gulp
-    .src(getGitFiles(/\.(mjs|js|jsx)$/), { base: "." })
+    .src(getGitFiles(SRC_SCRIPTS), SRC_CONFIG)
     .pipe(eslint())
-    .pipe(eslint.format("node_modules/eslint-formatter-pretty"))
+    .pipe(eslint.format(ESLINT_FORMATTER))
     .pipe(eslint.failAfterError())
 })
 
 gulp.task("pretty:js", () => {
   return gulp
-    .src(getGitFiles(/\.(mjs|js|jsx)$/), { base: "." })
+    .src(getGitFiles(SRC_SCRIPTS), SRC_CONFIG)
     .pipe(plumber())
-    .pipe(prettier({
-      printWidth: 111,
-      tabWidth: 2,
-      useTabs: false,
-      singleQuote: false,
-      trailingComma: "none",
-      jsxBracketSameLine: true,
-      bracketSpacing: true,
-      semi: false
-    }))
+    .pipe(prettier(PRETTIER_CONFIG))
     .pipe(eslint({ fix: true }))
-    .pipe(eslint.format("node_modules/eslint-formatter-pretty"))
+    .pipe(eslint.format(ESLINT_FORMATTER))
     .pipe(gulp.dest("."))
 })
 
 gulp.task("fix:js", () => {
   return gulp
-    .src(getGitFiles(/\.(mjs|js|jsx)$/), { base: "." })
+    .src(getGitFiles(SRC_SCRIPTS), SRC_CONFIG)
     .pipe(plumber())
     .pipe(eslint({ fix: true }))
-    .pipe(eslint.format("node_modules/eslint-formatter-pretty"))
+    .pipe(eslint.format(ESLINT_FORMATTER))
     .pipe(gulp.dest("."))
 })
